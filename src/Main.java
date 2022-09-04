a7d0d159f5b737f4986e97f0c3f0b09a86918a4c
Lena Niku <niku.elena@gmail.com>
2022-09-04 21:54:09 +0300

    Change type int to long

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main (String[] args) {
-        int[] sales = {22, 44, 66, 88};
+        long[] sales = {22, 44, 66, 88};
         SalesManager salesManager = new SalesManager(sales);
-        System.out.println("На сегодняя топ продаж " + salesManager.max());
+        System.out.println("На сегодня топ продаж " + salesManager.max());
     }
 }
